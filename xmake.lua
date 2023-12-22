dd21305a71350075a918c7396b7caa74c88ed50c
Kethers <49612587+Kethers@users.noreply.github.com>
2023-12-22 16:18:39 +0800

    Upload xmake.lua

---
+++ xmake.lua
@@ -0,0 +1,24 @@
+if is_mode("dist") then
+	set_optimize("fastest")
+	set_symbols("hidden")
+end
+
+BuildProject({
+	projectName = "Box2D",
+	projectType = "static",
+	macros = {},
+	languages = {"cxx20"},
+	depends = {},
+	files = {"src/**.cpp"},
+	headerfiles = {},
+	pchHeader = nil,
+	includePaths = {"include", "src"},
+	packages = {},
+	debugLink = {},
+	releaseLink = {},
+	link = {},
+	afterBuildFunc = nil,
+	enableException = true,
+	staticruntime = true,
+	group = "Dependencies",
+})
